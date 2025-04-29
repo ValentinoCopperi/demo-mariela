56fab3601c9257d99599ac28991c37ea421a2119
Valentino Copperi <copeguey@gmail.com>
2025-04-29 15:50:01 -0300

    Update ESLint configuration to disable various TypeScript and React rules for improved flexibility during development.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,33 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "prefer-const": "off",
+      "react/jsx-key": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react/no-unescaped-entities": "off",
+      "react/no-children-prop": "off",
+      "react/no-unstable-nested-components": "off",
+      "react/no-unknown-property": "off",
+      "react/no-unescaped-entities": "off",
+      "react/no-children-prop": "off",
+      "react/no-unstable-nested-components": "off",
+      "react/no-unknown-property": "off",
+      "react/no-array-index-key": "off",
+      "no-var": "off",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      "no-undef-init": "off",
+      "no-unused-imports": "off",
+      "no-unused-expressions": "off",
+      "react/display-name": "off"
+    },
+  },
 ];
 
 export default eslintConfig;
